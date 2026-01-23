9d22b3789505def2bb8d9e9bbd450f2e9e2bd698
Wolffy <2270617833@qq.com>
2026-01-23 10:00:03 +0800

    优化分享内容

--- src/components/blog/ShareButtons.tsx
+++ src/components/blog/ShareButtons.tsx
@@ -5,22 +5,23 @@ import { Button } from '@/components/ui/button';
 interface ShareButtonsProps {
     title: string;
     url: string;
+    description?: string;
 }
 
-export function ShareButtons({ title, url }: ShareButtonsProps) {
+export function ShareButtons({ title, url, description = '' }: ShareButtonsProps) {
     const [copied, setCopied] = React.useState(false);
 
     const encodedTitle = encodeURIComponent(title);
     const encodedUrl = encodeURIComponent(url);
+    const encodedDesc = encodeURIComponent(description || '');
+    const combinedText = encodeURIComponent(`${title}\n${description}`);
 
     const shareLinks = {
-        wechat: `https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${encodedUrl}`,
-        twitter: `https://twitter.com/intent/tweet?text=${encodedTitle}&url=${encodedUrl}`,
-        weibo: `https://service.weibo.com/share/share.php?title=${encodedTitle}&url=${encodedUrl}`,
-        qq: `https://connect.qq.com/widget/shareqq/index.html?url=${encodedUrl}&title=${encodedTitle}`,
-        facebook: `https://www.facebook.com/sharer/sharer.php?u=${encodedUrl}`,
-        telegram: `https://t.me/share/url?url=${encodedUrl}&text=${encodedTitle}`,
-        email: `mailto:?subject=${encodedTitle}&body=${url}`,
+        twitter: `https://twitter.com/intent/tweet?text=${combinedText}&url=${encodedUrl}`,
+        weibo: `https://service.weibo.com/share/share.php?title=${combinedText}&url=${encodedUrl}`,
+        facebook: `https://www.facebook.com/sharer/sharer.php?u=${encodedUrl}&quote=${encodedDesc}`,
+        telegram: `https://t.me/share/url?url=${encodedUrl}&text=${combinedText}`,
+        email: `mailto:?subject=${encodedTitle}&body=${encodedDesc}%0A%0A${encodedUrl}`,
     };
 
     const handleNativeShare = async () => {
@@ -28,7 +29,7 @@ export function ShareButtons({ title, url }: ShareButtonsProps) {
             try {
                 await navigator.share({
                     title: title,
-                    text: title,
+                    text: description || title,
                     url: url,
                 });
             } catch (err) {
@@ -40,6 +41,7 @@ export function ShareButtons({ title, url }: ShareButtonsProps) {
             alert('您的浏览器不支持系统分享');
         }
     };
+
 
     const copyLink = async () => {
         try {
@@ -66,34 +68,6 @@ export function ShareButtons({ title, url }: ShareButtonsProps) {
                 <Share2 className="h-4 w-4" />
             </Button>
 
-
-            {/* 微信分享 - 跳转到二维码页面 */}
-            <a href={shareLinks.wechat} target="_blank" rel="noopener noreferrer">
-                <Button
-                    variant="ghost"
-                    size="icon"
-                    className="h-9 w-9 rounded-full hover:bg-green-500/10 hover:text-green-500"
-                    title="微信扫码分享"
-                >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
-                        <path d="M8.691 2.188C3.891 2.188 0 5.476 0 9.53c0 2.212 1.17 4.203 3.002 5.55a.59.59 0 0 1 .213.665l-.39 1.48c-.019.07-.048.141-.048.213 0 .163.13.295.29.295a.326.326 0 0 0 .167-.054l1.903-1.114a.864.864 0 0 1 .717-.098 10.16 10.16 0 0 0 2.837.403c.276 0 .543-.027.811-.05-.857-2.578.157-4.972 1.932-6.446 1.703-1.415 3.882-1.98 5.853-1.838-.576-3.583-4.196-6.348-8.596-6.348zM5.785 5.991c.642 0 1.162.529 1.162 1.18a1.17 1.17 0 0 1-1.162 1.178A1.17 1.17 0 0 1 4.623 7.17c0-.651.52-1.18 1.162-1.18zm5.813 0c.642 0 1.162.529 1.162 1.18a1.17 1.17 0 0 1-1.162 1.178 1.17 1.17 0 0 1-1.162-1.178c0-.651.52-1.18 1.162-1.18zm5.34 2.867c-1.797-.052-3.746.512-5.28 1.786-1.72 1.428-2.687 3.72-1.78 6.22.942 2.453 3.666 4.229 6.884 4.229.826 0 1.622-.12 2.361-.336a.722.722 0 0 1 .598.082l1.584.926a.272.272 0 0 0 .14.047c.134 0 .24-.111.24-.247 0-.06-.023-.12-.038-.177l-.327-1.233a.582.582 0 0 1-.023-.156.49.49 0 0 1 .201-.398C23.024 18.48 24 16.82 24 14.98c0-3.21-2.931-5.837-6.656-6.088V8.89c-.135-.01-.269-.03-.406-.03zm-2.53 3.274c.535 0 .969.44.969.982a.976.976 0 0 1-.969.983.976.976 0 0 1-.969-.983c0-.542.434-.982.97-.982zm4.844 0c.535 0 .969.44.969.982a.976.976 0 0 1-.969.983.976.976 0 0 1-.969-.983c0-.542.434-.982.969-.982z" />
-                    </svg>
-                </Button>
-            </a>
-
-            {/* QQ 分享 */}
-            <a href={shareLinks.qq} target="_blank" rel="noopener noreferrer">
-                <Button
-                    variant="ghost"
-                    size="icon"
-                    className="h-9 w-9 rounded-full hover:bg-blue-500/10 hover:text-blue-500"
-                    title="分享到 QQ"
-                >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
-                        <path d="M12.003 2c-2.265 0-6.29 1.364-6.29 7.325v1.195S3.55 14.96 3.55 17.474c0 .665.17 1.025.281 1.025.114 0 .902-.484 1.748-2.072 0 0-.18 2.197 1.904 3.967 0 0-1.77.495-1.77 1.182 0 .686 4.078.43 6.29.43 2.211 0 6.29.256 6.29-.43 0-.687-1.77-1.182-1.77-1.182 2.085-1.77 1.904-3.967 1.904-3.967.846 1.588 1.634 2.072 1.748 2.072.111 0 .281-.36.281-1.025 0-2.514-2.166-6.954-2.166-6.954V9.325C18.29 3.364 14.268 2 12.003 2z" />
-                    </svg>
-                </Button>
-            </a>
 
             {/* 微博分享 */}
             <a href={shareLinks.weibo} target="_blank" rel="noopener noreferrer">
